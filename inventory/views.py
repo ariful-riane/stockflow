f716da5bf133bcd23b4e88d6a9c8553c93ac3c32
Joel Soloman <solomanjoel41@gmail.com>
2026-08-11 14:12:05 +0100

    added base layout and base layout

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, "inventory/product_list.html", 
+                  {"products": products})
+
+
