df8999f5513c4baf75c54b7d1f2471e617bb7d97
Joel Soloman <solomanjoel41@gmail.com>
2026-08-11 18:22:59 +0100

    Add product creation page

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import ProductForm
 from .models import Product
 
 def product_list(request):
@@ -7,4 +8,15 @@ def product_list(request):
     return render(request, "inventory/product_list.html", 
                   {"products": products})
 
-
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("inventory:product_list")
+    else:
+        form = ProductForm()
+    return render(request, 
+                    "inventory/product_form.html",
+                    {"form": form}
+                )
