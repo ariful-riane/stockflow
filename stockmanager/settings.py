d39c3e07d55045bcbd95b4490d28ee94edbf340a
Ariful Riane <arifulrian@gmail.com>
2026-08-14 10:45:04 +0000

    csrf token and login redirection

--- stockmanager/settings.py
+++ stockmanager/settings.py
@@ -25,8 +25,13 @@ SECRET_KEY = 'django-insecure-+f2+@83+xd@kvedj$ys#mcr-ru9cstigbwbj8%0=sca10yee%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost:8000",
+]
 
 # Application definition
 
@@ -127,6 +132,6 @@ MAILERS = {
     },
 }
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'index'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/products/'
+LOGOUT_REDIRECT_URL = '/'
